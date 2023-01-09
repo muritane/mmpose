c4a914e8fe38902f253861d4b4ea07d76f5625d2
tamer <tamer@ids-ki4mrk-02>
2023-01-09 12:52:08 +0100

    added ros_nodes

--- mmpose/apis/webcam/nodes/__init__.py
+++ mmpose/apis/webcam/nodes/__init__.py
@@ -3,6 +3,7 @@ from .base_visualizer_node import BaseVisualizerNode
 from .helper_nodes import MonitorNode, ObjectAssignerNode, RecorderNode
 from .model_nodes import (DetectorNode, PoseTrackerNode,
                           TopDownPoseEstimatorNode)
+from .ros_nodes import ModelInferenceROSNode
 from .node import Node
 from .registry import NODES
 from .visualizer_nodes import (BigeyeEffectNode, NoticeBoardNode,
@@ -12,5 +13,6 @@ __all__ = [
     'BaseVisualizerNode', 'NODES', 'MonitorNode', 'ObjectAssignerNode',
     'RecorderNode', 'DetectorNode', 'PoseTrackerNode',
     'TopDownPoseEstimatorNode', 'Node', 'BigeyeEffectNode', 'NoticeBoardNode',
-    'ObjectVisualizerNode', 'ObjectAssignerNode', 'SunglassesEffectNode'
+    'ObjectVisualizerNode', 'ObjectAssignerNode', 'SunglassesEffectNode',
+    'ModelInferenceROSNode'
 ]
---
+++ mmpose/apis/webcam/nodes/ros_nodes/model_inference_ros_node.py
@@ -0,0 +1,67 @@
+import time
+from typing import List, Union
+
+import os
+import rospy
+from std_msgs.msg import String
+import json
+
+from mmpose.utils.timer import RunningAverage
+from ..node import Node
+from ..registry import NODES
+
+
+@NODES.register_module()
+class ModelInferenceROSNode(Node):
+    """Publish model inference of PoseTrackerNode as std_msgs/String msg."""
+
+    def __init__(self, name: str, frame_buffer: str, object_buffer: str,
+                 topic: str):
+        super().__init__(name=name, enable=True)
+        self.synchronous = None
+
+        # Register buffers
+        # The trigger buffer depends on the executor.synchronous attribute,
+        # so it will be set later after the executor is assigned in
+        # ``set_executor``.
+        self.register_input_buffer(object_buffer, 'object', trigger=False)
+        self.register_input_buffer(frame_buffer, 'frame', trigger=False)
+        
+        rospy.init_node(name)
+        self.model_inference_pub = rospy.Publisher(topic, String, queue_size=10)
+
+    def set_executor(self, executor):
+        super().set_executor(executor)
+        # Set synchronous according to the executor
+        if executor.synchronous:
+            self.synchronous = True
+            trigger = 'object'
+        else:
+            self.synchronous = False
+            trigger = 'frame'
+
+        # Set trigger input buffer according to the synchronous setting
+        for buffer_info in self._input_buffers:
+            if buffer_info.input_name == trigger:
+                buffer_info.trigger = True
+
+    def process(self, input_msgs):
+        if rospy.is_shutdown():
+            print("ROS is shutdown -> exit")
+            self._event_manager.set('_exit_')
+
+        object_msg = input_msgs['object']
+        objects = [] if object_msg is None else object_msg.get_objects()
+
+        labels = [d['label'] for d in objects]
+        rospy.loginfo("labels: {}".format(labels))
+        if "person" in labels:
+            rospy.loginfo([d for d in objects if d['label'] == 'person'])
+            self._event_manager.set('_exit_')
+
+        # self.model_inference_pub.publish(str(objects))
+
+    def _get_node_info(self):
+        info = super()._get_node_info()
+        return info
+
